505a1fd2058ecab4b01ff5ddc2176d76f3f75a81
CrBoy <crboy@crboy.net>
2018-03-13 03:47:23 +0800

    Add two options: ignore_cert and insecure, which are default false

--- lib/kotsms.rb
+++ lib/kotsms.rb
@@ -3,10 +3,11 @@ require "uri"
 require "iconv"
 
 class Kotsms
-	SMS_ENDPOINT = "https://api.kotsms.com.tw/kotsmsapi-1.php"
-	BULK_SMS_ENDPOINT = "https://api.kotsms.com.tw/kotsmsapi-2.php"
-	BALANCE_ENDPOOINT = "http://mail2sms.com.tw/memberpoint.php"
-	STATUS_ENDPOOINT = "http://mail2sms.com.tw/msgstatus.php"
+	API_HOST = "api.kotsms.com.tw"
+	SMS_ENDPOINT = "/kotsmsapi-1.php"
+	BULK_SMS_ENDPOINT = "/kotsmsapi-2.php"
+	BALANCE_ENDPOOINT = "/memberpoint.php"
+	STATUS_ENDPOOINT = "/msgstatus.php"
 
 	def initialize(username, password)
 		login(username, password)
@@ -18,8 +19,18 @@ class Kotsms
 		true
 	end
 
-	def deliver(dst, content, options={})
-		endpoint = case (options[:mode].to_sym rescue nil)
+	# recipient (string) - sms recipient in general format; e.g. '+886912345678'
+	# message (string) - message content
+	# options (hash) - optional config
+	# options.ignore_cert (boolean) - Ignore SSL certificate or not
+	# options.insecure (boolean) - Use plain HTTP or HTTPS
+	# options.mode (string) - delivery mode
+	#   'bit' - instant delivery (default)
+	#   'bulk' - bulk delivery
+	def deliver(recipient, message, options={})
+		protocol = options[:insecure] ? "http" : "https"
+		uri = URI.parse "#{protocol}://#{API_HOST}"
+		uri.path = case (options[:mode].to_sym rescue nil)
 				   when nil, :bit
 					   SMS_ENDPOINT
 				   when :bulk
@@ -27,25 +38,28 @@ class Kotsms
 				   else
 					   raise StandardError.new "Bad delivering mode!"
 				   end
-		uri = URI.parse(endpoint)
 
 		uri.query = URI.encode_www_form({
 			username: @username,
 			password: @password,
-			dstaddr: dst,
-			smbody: Iconv.new("big5", "utf-8").iconv(content),
+			dstaddr: recipient,
+			smbody: Iconv.new("big5", "utf-8").iconv(message),
 			dlvtime: (options[:dlvtime] rescue 0),
 			vldtime: (options[:vldtime] rescue nil),
 			response: (options[:response] rescue nil)
 		})
 
-		response = Net::HTTP.get_response(uri)
+		response = Net::HTTP.start(uri.host, use_ssl: uri.scheme == 'https') do |http|
+			http.verify_mode = OpenSSL::SSL::VERIFY_NONE if options[:ignore_cert]
+			req = Net::HTTP::Get.new uri
+			http.request(req)
+		end
 
 		parse_response(response.body)["kmsgid"].to_i
 	end
 
-	def deliver_bulk(dst, content, options={})
-		deliver(dst, content, options.merge(mode: :bulk))
+	def deliver_bulk(recipient, message, options={})
+		deliver(recipient, message, options.merge(mode: :bulk))
 	end
 
 	def balance
